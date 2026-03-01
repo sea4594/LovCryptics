24fa7ae27fae7d012f682d04d767dcefee49a439
sea4594 <sea4594@gmail.com>
2026-03-01 04:03:31 -0500

    Implement service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = "cryptic-cache-v1";
+const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./idb.js", "./manifest.webmanifest"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((k) => (k === CACHE ? null : caches.delete(k)))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  const url = new URL(e.request.url);
+  if (url.origin === location.origin) {
+    e.respondWith(
+      caches.match(e.request).then((cached) => cached || fetch(e.request).then((resp) => {
+        const copy = resp.clone();
+        caches.open(CACHE).then((c) => c.put(e.request, copy));
+        return resp;
+      }))
+    );
+  }
+});
